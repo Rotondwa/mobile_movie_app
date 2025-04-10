a82920ff9e9b4b4c74412cf767e754fe177fa7ce
Rotondwa <rotondwamk9864@gmail.com>
2025-04-11 00:02:36 +0200

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome! This is a test wow</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
